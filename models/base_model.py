89f140acff394aa41125c8ea6acf09b65a42d476
Nicolas <nzparra@gmail.com>
2020-02-13 19:52:17 -0500

    FIRST COMMIT ADD FileStorage that serializes instances to a JSON file and deserializes JSON file ADD ALL CLASSES STATE CITY AMENITY PLACE REVIEW BASE_MODEL ADD TEST FILES

---
+++ models/base_model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Base model of AirBnB"""
+from datetime import datetime
+from uuid import uuid4 as idc
+import models
+
+
+class BaseModel:
+    """defines all common attributes/methods for other classes"""
+    def __init__(self, **kwargs):
+        """Init base model
+            kwargs: Arguments
+            id: unique id
+            created_at: creation datetime
+            updated_at: updated datetime
+        """
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(idc())
+            self.created_at = self.updated_at = datetime.now()
+            models.write.new(self)
+
+    def __str__(self):
+        """ Return a string with
+            class name, id, dict
+        """
+        return ("[{}] ({}) {}".format(type(self).__name__,
+                                      self.id,
+                                      self.__dict__))
+
+    def save(self):
+        """updates the public instance attribute updated_at
+        with the current datetime"""
+        self.updated_at = datetime.now()
+        models.write.save()
+
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values"""
+        dic = dict(self.__dict__)
+        dic["__class__"] = str(type(self).__name__)
+        dic["created_at"] = self.created_at.isoformat()
+        dic["updated_at"] = self.updated_at.isoformat()
+        return (dic)
---
+++ models/engine/file_storage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Storage for AirBnb"""
+import json
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ returns the dictionary __objects"""
+        return (self.__objects)
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        dic = {}
+        for key, value in self.__objects.items():
+            dic[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as file:
+            json.dump(dic, file)
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path, 'r', encoding="UTF-8") as file:
+                for key, value in (json.load(file)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
+        except OSError:
+            pass
