48572fd5f2dca1ec61b3a6f9e29dc4ce3477f642
sebastianchc <1200@holbertonschool.com>
2020-02-18 13:29:19 -0500

    Checkers

--- models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""City class"""
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    """Class city"""
-    state_id = ""
-    name = ""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,43 +1,40 @@
 #!/usr/bin/python3
-"""Storage for AirBnb"""
+""" FileStorage class of AirBnB clone project """
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 
 class FileStorage:
+    """ FileStorage class """
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """ returns the dictionary __objects"""
-        return (self.__objects)
+        """ all method """
+        return FileStorage.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        if obj:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            self.__objects[key] = obj
+        """ new method """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
-        dic = {}
-        for key, value in self.__objects.items():
-            dic[key] = value.to_dict()
-        with open(self.__file_path, 'w', encoding="UTF-8") as file:
-            json.dump(dic, file)
+        """ save method """
+        JSON_dict = {}
+        for key, value in FileStorage.__objects.items():
+            JSON_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(JSON_dict, file)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
+        """ reload method """
         try:
-            with open(self.__file_path, 'r', encoding="UTF-8") as file:
-                for key, value in (json.load(file)).items():
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                from models.base_model import BaseModel
+                from models.user import User
+                normal_dict = json.load(file)
+                for key, value in normal_dict.items():
                     value = eval(value["__class__"])(**value)
-                    self.__objects[key] = value
+                    FileStorage.__objects[key] = value
         except OSError:
             pass
