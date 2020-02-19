27788a7512aba270ab7039261f155b94471ed89f
Nicolas <nzparra@gmail.com>
2020-02-19 18:20:52 -0500

    Add Unittest to BaseModel

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -3,23 +3,44 @@
 import inspect
 import unittest
 import pep8
-from models import base_model
+from datetime import datetime
+from models.base_model import BaseModel
 
 
-class test_scripts(unittest.TestCase):
-    @classmethod
-    def test_class(cls):
-        cls.base_model = inspect.getmembers(base_model, inspect.isfunction)
+class test_base_model(unittest.TestCase):
 
-    def test_pep8(self):
-        style = pep8.StyleGuide(quiet=True)
-        models_base = style.check_files(['models/base_model.py'])
-        self.assertEqual(models_base.total_errors, 0,
-                         "Found code style errors(and warnings).")
+    def test_ids(self):
+        """Verify's exists different ids"""
+        base1 = BaseModel()
+        base2 = BaseModel()
+        base3 = BaseModel()
+        self.assertFalse(base1.id == base2.id)
+        self.assertFalse(base1.id == base3.id)
+        self.assertFalse(base2.id == base3.id)
 
-    def test_module_documentation(self):
-        self.assertTrue(len(base_model.__doc__) > 0)
+    def test_created_and_updated(self):
+        """Test the updated time"""
+        base1 = BaseModel()
+        created = base1.created_at
+        updated = base1.updated_at
+        base1.save()
+        self.assertFalse(updated == base1.updated_at)
+        self.assertTrue(created == base1.created_at)
 
-    def test_functions(self):
-        for i in self.base_model:
-            self.assertTrue(len(i[1].__doc__) > 0)
+    def test_iso(self):
+        base1 = BaseModel()
+        cre = upd = datetime.now()
+        base1.created_at = cre
+        base1.updated_at = upd
+        d = base1.to_dict()
+        self.assertEqual(d["created_at"], cre.isoformat())
+        self.assertEqual(d["updated_at"], upd.isoformat())
+
+    def test_dict_date(self):
+        base1 = BaseModel()
+        d = base1.to_dict()
+        self.assertEqual(d["__class__"], "BaseModel")
+        self.assertEqual(type(d["created_at"]), str)
+
+
+
